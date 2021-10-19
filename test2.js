87e7c29c1476f7c9ef88bcc6a63ebe9705dd6fa7
Ozzipozzo <ghanmiabdallahb@gmail.com>
2021-10-19 11:11:14 +0200

    [ADD] test 2 done

--- test2.js
+++ test2.js
@@ -20,9 +20,55 @@ const input = [
   ]
 
 
+  const transform = x => {
+      
+    let tab = []
+    let tab2 = []
+
+
+    // check is the price is undefined or not
+    x.forEach(e => {
+        if(e.price) {
+            tab.push(e)
+        }
+    });
+
+
+    tab.forEach(e => {
+        switch (e.savingsType) {
+            case "total":
+                let price = e.price - e.savings
+                tab2.push({
+                    "basePrice": '3000 €',
+                    "description": `Vous économisez ${e.savings} € par rapport au prix initial`,
+                    "price": price.toString() + " €",
+                    "savings": e.savings.toString() + " €",
+                    "title": 'Bon de reduction'
+                });
+            break;
+            
+            case "percent":
+                let totalSavings = (e.savings / 100) * e.price
+                let price2 = e.price - totalSavings
+                tab2.push({
+                    "basePrice": '3000 €',
+                    "description": `Vous économisez ${totalSavings} € par rapport au prix initial`,
+                    "price": price2.toString() + " €",
+                    "savings": e.savings.toString() + " %",
+                    "title": 'Soldes'
+                });
+            break;
+                default: 
+                console.log("Veuillez renseigner un tableau d'object");
+        }
+    });
+    
+    console.log(tab2)
+   
+}
+
   transform(input)
 
-  
   // output :
   // [
   //   {
